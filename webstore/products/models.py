a8fbfb90bdfec2a4754c838123f73c3147d9dddf
Jorge Luis Escobedo Ocaña <jescobedooc@unsa.edu.pe>
2023-07-28 17:10:49 -0500

    avance en la app products

--- webstore/products/models.py
+++ webstore/products/models.py
@@ -4,3 +4,5 @@ class Productos(models.Model):
     nombre = models.CharField(max_length=200)
     descripcion = models.TextField()
     precio = models.CharField(max_length=100)
+    def __str__(self):
+        return self.nombre
