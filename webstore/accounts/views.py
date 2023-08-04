8d9ccff6dca93d4f3f971cb57e60f4983650bb1d
William <wroquequi@unsa.edu.pe>
2023-08-03 22:57:01 -0500

    Cambios en los estilos, mejoras en la presentacion de las ventas y productos, al igual que el registŕo e inicio de sesión.

--- webstore/accounts/views.py
+++ webstore/accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def register(request):
     if request.method == 'POST':
